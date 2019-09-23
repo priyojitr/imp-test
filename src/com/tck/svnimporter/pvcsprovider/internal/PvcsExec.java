1c0cd75c6cd00114db8a69d8a828df962d78328f
priyojitr <roypriyojit@gmail.com>
2019-09-24 01:43:13 +0530

    updated erroneous section

--- src/com/tck/svnimporter/pvcsprovider/internal/PvcsExec.java
+++ src/com/tck/svnimporter/pvcsprovider/internal/PvcsExec.java
@@ -2,8 +2,7 @@ package com.tck.svnimporter.pvcsprovider.internal;
 
 import org.polarion.svnimporter.common.Exec;
 import org.polarion.svnimporter.common.Log;
-import com.tck.svnimporter.pvcsprovider.internal.PvcsExec.1;
-import com.tck.svnimporter.pvcsprovider.internal.PvcsExec.2;
+import org.polarion.svnimporter.common.StreamConsumer;
 
 public class PvcsExec extends Exec {
 	private static final Log LOG = Log.getLog(PvcsExec.class);
@@ -14,14 +13,22 @@ public class PvcsExec extends Exec {
 	}
 
 	protected void setupProcessStderr(Process process) throws Exception {
-      this.setStderrConsumer(new 1(this));
-      super.setupProcessStderr(process);
-   }
+		this.setStderrConsumer((StreamConsumer) new StreamConsumer() {
+			public void consumeLine(final String line) {
+				PvcsExec.this.checkProjectPathError(line);
+			}
+		});
+		super.setupProcessStderr(process);
+	}
 
 	protected void setupProcessStdout(Process process) throws Exception {
-      this.setStdoutConsumer(new 2(this));
-      super.setupProcessStdout(process);
-   }
+		this.setStdoutConsumer((StreamConsumer) new StreamConsumer() {
+			public void consumeLine(final String line) {
+				PvcsExec.this.checkProjectPathError(line);
+			}
+		});
+		super.setupProcessStdout(process);
+	}
 
 	private void checkProjectPathError(String line) {
 		if (line.startsWith("The project root could not be loaded")) {
